b9045c219ad718031d0349939720154a8e6aadc0
Kevin McLarnon <mclarnon.k@gmail.com>
2017-06-17 11:28:32 -0400

    start adding functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
+'use strict';
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database. 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.computeCrimePercentages = functions.database.ref('crimedata/{pushId}')
+  .onWrite(event => {
+    console.log('Recalculating crime percentages for new message', event.params.pushId);    
+  });
+
+  
