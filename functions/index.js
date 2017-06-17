8253c57725a0844cf45397ca9fb01b80cb129486
Kevin McLarnon <mclarnon.k@gmail.com>
2017-06-17 11:42:20 -0400

    severity

--- functions/index.js
+++ functions/index.js
@@ -12,4 +12,11 @@ exports.computeCrimePercentages = functions.database.ref('crimedata/{pushId}')
     console.log('Recalculating crime percentages for new message', event.params.pushId);    
   });
 
+exports.computeSeverityLevel = functions.database.ref('crimedata/pushId}/offense_code_group')
+  .onWrite(event => {
+    console.log('Calculating severity level for', event.params.pushId);
+    
+    return event.data.ref.parent.child('severity').set("Something");  
+  });
+
   
