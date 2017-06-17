56dfc6ffd62d75e49319953ce5f20472b86c2dd3
Alex Karl Voskuil <avoskuil@andrew.cmu.edu>
2017-06-17 15:56:34 -0400

    change weights

--- functions/index.js
+++ functions/index.js
@@ -5,22 +5,22 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database. 
 const admin = require('firebase-admin');
-var severDict = {'Larceny From Motor Vehicle' : 0.1, 'Harassment' : 0.2, 'Missing Person Reported ' : 0.3, 
-                 'Larceny' : 0.4, 'Robbery' : 0.5, 'Vandalism' : 0.6, 'Investigate Property': 0.7,
-                 'Simple Assault' : 0.8, 'Verbal Disputes' : 0.9, 'Medical Assistance' : 0.1, 
-                 'Violations' : 0.1, 'Drug Violation' : 0.2, 'Aggravated Assault' : 0.3,
-                 'Property Lost' : 0.4, 'Police Service Incidents' : 0.5, 'Towed' : 0.6,
-                 'Warrant Arrests' : 0.7, 'Counterfeiting' : 0.8, 'Other' : 0.9,
-                 'Motor Vehicle Accident Response  Disorderly Conduct' : 0.2,              
-                 'Auto Theft' : 0.1, 'Recovered Stolen Property' : 0.2, 'Restraining Order Violations' : 0.3,    
-                 'Firearm Violations' : 0.4, 'Commercial Burglary' : 0.5, 'Investigate Person' : 0.6,             
-                 'Residential Burglary' : 0.7, 'Missing Person Located' : 0.8, 'Sex Offender Registration' : 0.9,       
-                 'Fraud' : 0.3, 'Confidence Games' : 0.1, 'Evading Fare' : 0.2, 'Auto Theft Recovery' : 0.3,
-                 'Assembly or Gathering Violations Other Burglary' : 0.4, 'Indecent Assault' : 0.5,
-                 'Property Found' : 0.6, 'License Violation' : 0.7, 'Landlord/Tenant Disputes' : 0.8,
-                 'Fire Related Reports' : 0.9, 'Operating Under the Influence' : 0.4, 'Service' : 0.1,
-                 'Search Warrants' : 0.2, 'Property Related Damage' : 0.3, 'Prisoner Related Incidents' : 0.4,
-                 'Offenses Against Child / Family  Harbor Related Incidents' : 0.5 }
+var severDict = {'Larceny From Motor Vehicle' : 0.7, 'Harassment' : 0.8, 'Missing Person Reported ' : 0.1, 
+                 'Larceny' : 0.7, 'Robbery' : 0.7, 'Vandalism' : 0.6, 'Investigate Property': 0.1,
+                 'Simple Assault' : 0.8, 'Verbal Disputes' : 0.4, 'Medical Assistance' : 0.6, 
+                 'Violations' : 0.5, 'Drug Violation' : 0.5, 'Aggravated Assault' : 0.8,
+                 'Property Lost' : 0.1, 'Police Service Incidents' : 0.5, 'Towed' : 0.2,
+                 'Warrant Arrests' : 0.7, 'Counterfeiting' : 0.3, 'Other' : 0.5,
+                 'Motor Vehicle Accident Response  Disorderly Conduct' : 0.7,              
+                 'Auto Theft' : 0.7, 'Recovered Stolen Property' : 0.1, 'Restraining Order Violations' : 0.4,    
+                 'Firearm Violations' : 0.7, 'Commercial Burglary' : 0.7, 'Investigate Person' : 0.1,             
+                 'Residential Burglary' : 0.7, 'Missing Person Located' : 0.1, 'Sex Offender Registration' : 0.9,       
+                 'Fraud' : 0.3, 'Confidence Games' : 0.1, 'Evading Fare' : 0.2, 'Auto Theft Recovery' : 0.8,
+                 'Assembly or Gathering Violations Other Burglary' : 0.7, 'Indecent Assault' : 0.9,
+                 'Property Found' : 0.1, 'License Violation' : 0.3, 'Landlord/Tenant Disputes' : 0.4,
+                 'Fire Related Reports' : 0.9, 'Operating Under the Influence' : 0.6, 'Service' : 0.1,
+                 'Search Warrants' : 0.2, 'Property Related Damage' : 0.6, 'Prisoner Related Incidents' : 0.9,
+                 'Offenses Against Child / Family  Harbor Related Incidents' : 0.9 }
 admin.initializeApp(functions.config().firebase);
 
 exports.computeCrimePercentages = functions.database.ref('crimedata/{pushId}/offense_code_group')
